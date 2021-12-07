234cdcb61b80dcc3c756bdcd259882a66fca1dba
Ulrik Laurén <ulrik.lauren@gmail.com>
2021-12-07 10:46:57 +0100

    Adding extended debug to find errors in configuration
    The configuration is now checked for duplicated table IDs and also for options using
    undefined table IDs.

--- app/src/main/java/com/example/ruinmasterstables/MainActivity.kt
+++ app/src/main/java/com/example/ruinmasterstables/MainActivity.kt
@@ -25,15 +25,17 @@ data class Options (var chance : Int, var table : ArrayList<Int>, var text : Str
 
 private const val ENCOUNTER_TABLE_OFFSET = 25
 private const val TREASURE_TABLE_OFFSET = 50
+private const val CONFIG_FILE_TABLE_ID_OFFSET = 100
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private inline fun <reified T> ObjectMapper.readValue(s: String): T = this.readValue(s, object : TypeReference<T>() {})
     private val buttonId : Int =  View.generateViewId()
     private lateinit var configData : ConfigurationData
+    private var configFiles = ArrayList<String>()
 
     override fun onClick(v: View) {
         if (v.id == buttonId) {
-            debug("Clicked button for table ${v.tag}")
+            debug("Clicked button to trigger table ${getActualTableId(v.tag as Int)} in '${getConfigFileName(v.tag as Int)}'.")
             var terrainText : String = triggerTable(configData.tables, v.tag as Int)
             terrainText = terrainText.trimStart()
             if (terrainText.isNotBlank()) terrainText = replaceDieRolls(terrainText)
@@ -58,16 +60,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         if (files != null) {
             for ((index, file) in files.withIndex()) {
                 if (file.endsWith(".json")) {
-                    debug("Found file named '${file.toString()}'")
-                    var tempConfigData = loadConfiguration(assets.open(file))
+                    debug("Found configuration file named '${file.toString()}'")
+                    configFiles += file.toString()
+                    val tempConfigData = loadConfiguration(assets.open(file))
                     //Each file get an offset to allow multiple files. This is done to make it easy to split the files
-                    tempConfigData.buttons.forEach {
-                        it.table += index*100
+                    tempConfigData.buttons.forEach { button ->
+                        button.table += index*CONFIG_FILE_TABLE_ID_OFFSET
                     }
-                    tempConfigData.tables.forEach {
-                        it.id += index*100
-                        it.options.forEach {
-                            it.table.forEachIndexed { pos, value -> it.table[pos] = value + index*100 }
+                    tempConfigData.tables.forEach { table ->
+                        table.id += index*CONFIG_FILE_TABLE_ID_OFFSET
+                        table.options.forEach { option ->
+                            option.table.forEachIndexed { pos, value -> option.table[pos] = value + index*CONFIG_FILE_TABLE_ID_OFFSET }
                         }
                     }
 
@@ -81,8 +84,86 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 }
             }
         }
+        isConfigurationDataValid(configData)
         //Draw the UI based on the loaded data
         createButtons(configData.buttons)
+    }
+
+    private fun isConfigurationDataValid( configData : ConfigurationData) {
+        val allIds = ArrayList<Int>()
+        val allTableOptions = ArrayList<Int>()
+
+        configData.tables.forEach{ table ->
+            allIds += table.id
+            table.options.forEach{ option ->
+                if (option.table.isNotEmpty()) {
+                    allTableOptions += option.table
+                }
+            }
+        }
+        if (allIds.size != allIds.distinct().count()) {
+            val duplicatedIds = allIds.groupingBy { it }.eachCount().filter { it.value > 1 }
+
+            error("There are duplicated table IDs!")
+            duplicatedIds.forEach {
+                error(getTableNameFromId(it.key))
+            }
+        }
+        val allDistinctTableOptions = allTableOptions.distinct()
+        val missingTableIds =  ArrayList<Int>()
+        allDistinctTableOptions.forEach{
+            if (it !in allIds) {
+                missingTableIds += it
+            }
+        }
+        if (missingTableIds.isNotEmpty()) {
+            error("There are options using undefined table IDs!")
+            missingTableIds.forEach {
+                error(getTableNameFromUsedOptionsTableReferences(it))
+            }
+        }
+    }
+
+    private fun getTableNameFromId(id : Int) : String {
+        var foundNames = String()
+        if (this::configData.isInitialized) {
+            foundNames = "ID ${getActualTableId(id)} is used by "
+            configData.tables.forEach{ table ->
+                if (table.id == id) {
+                    foundNames += "'${table.name}' "
+                }
+            }
+            foundNames += "in '${getConfigFileName(id)}'."
+        }
+        return foundNames
+    }
+
+    private fun getTableNameFromUsedOptionsTableReferences(tableId : Int) : String {
+        var foundNames = String()
+        if (this::configData.isInitialized) {
+            foundNames = "Undefined ID ${getActualTableId(tableId)} is used by "
+            configData.tables.forEach{ table ->
+                table.options.forEach { option ->
+                    option.table.forEach { id ->
+                        if (id == tableId) {
+                            foundNames += "'${table.name}' "
+                        }
+                    }
+                }
+            }
+            foundNames += "in '${getConfigFileName(tableId)}'."
+        }
+        return foundNames
+    }
+
+    private fun getActualTableId(tableId : Int) : Int {
+        val configFileIndex = (tableId / CONFIG_FILE_TABLE_ID_OFFSET) - 1
+        return (tableId - CONFIG_FILE_TABLE_ID_OFFSET * configFileIndex)
+    }
+
+    private fun getConfigFileName(tableId : Int) : String {
+        val configFileIndex = (tableId / CONFIG_FILE_TABLE_ID_OFFSET) - 1
+        return configFiles.elementAtOrElse(configFileIndex){"unknown file"}
     }
 
     private fun loadConfiguration( configStream: InputStream ) : ConfigurationData {
@@ -94,12 +175,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private fun createButtons(buttons : List<Buttons>) {
         val linearLayout = findViewById<View>(R.id.buttons_layout) as LinearLayout //a constraint layout pre-made in design view
 
-        buttons.forEach {
+        buttons.forEach { button ->
             val dynamicButton = Button(this)
             dynamicButton.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-            dynamicButton.text = it.text
+            dynamicButton.text = button.text
             dynamicButton.id = buttonId
-            dynamicButton.tag = it.table
+            dynamicButton.tag = button.table
             dynamicButton.minimumWidth = 500
             dynamicButton.setOnClickListener(this)
             linearLayout.addView(dynamicButton)
@@ -113,7 +194,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         for (table in tables) {
             if (table.id == tableId) {
                 foundTable = true
-                debug("Found table named '${table.name}' with id=$tableId")
+                debug("Found table named '${table.name}' with id=${getActualTableId(tableId)} in '${getConfigFileName(tableId)}'.")
                 val optionResult : Pair<List<Int>,String> = getRandomOption(table.options)
 
                 if (optionResult.second.isNotBlank()) {
@@ -121,9 +202,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 }
 
                 if (optionResult.first.isNotEmpty()) {
-                    optionResult.first.forEach {
-                        debug("Continue with table id=$it")
-                        val tableText : String = triggerTable(tables, it)
+                    optionResult.first.forEach { id ->
+                        debug("Continue with table id=${getActualTableId(id)} in '${getConfigFileName(id)}'.")
+                        val tableText : String = triggerTable(tables, id)
                         if (tableText.isNotBlank()) {
                             resultingText += if (resultingText.isNotBlank()) { " $tableText" } else { tableText }
                         }
@@ -133,7 +214,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
         }
 
-        if (!foundTable) warning("Did not find a table with id=$tableId")
+        if (!foundTable) warning("Did not find a table with id=${getActualTableId(tableId)} in '${getConfigFileName(tableId)}'.")
         return resultingText
     }
 
@@ -141,8 +222,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         var sumOfAllChance = 0
         var sumOfChance = 0
 
-        options.forEach {
-            sumOfAllChance += it.chance
+        options.forEach { option ->
+            sumOfAllChance += option.chance
         }
         val randomChance : Int = getRandomInt(1, sumOfAllChance)
         debug("Got $randomChance out of $sumOfAllChance when getting a random option")
@@ -184,6 +265,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private fun showEditDialog(terrainText: String, encounterText: String, treasureText: String) {
         val displayResultFragment: DisplayResultFragment = DisplayResultFragment.newInstance(terrainText, encounterText, treasureText)
         displayResultFragment.show(supportFragmentManager, "fragment_display_result")
+    }
+
+    private fun error(message: String) {
+        Log.e("RuinMastersTables::MainActivity", message)
     }
 
     private fun warning(message: String) {
