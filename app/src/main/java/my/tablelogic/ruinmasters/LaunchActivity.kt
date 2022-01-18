72646027343472ce67cf8f8d3399476546eeb7eb
Ulrik Laurén <ulrik.lauren@gmail.com>
2022-01-18 21:56:05 +0100

    Add splash screen at start

---
+++ app/src/main/java/my/tablelogic/ruinmasters/LaunchActivity.kt
@@ -0,0 +1,19 @@
+// Copyright (c) 2021 Ulrik Laurén
+// Part of RuinMastersTables
+// MIT License, see LICENSE file
+package my.tablelogic.ruinmasters
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LaunchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Send user to MainActivity as soon as this activity loads
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        // remove this activity from the stack
+        finish()
+    }
+}
